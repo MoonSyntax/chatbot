e04b6cc673532e0775dc16fdf75546f064b242c8
MoonSyntax <ranjanprainila2809@gmail.com>
2025-03-16 12:51:55 +0530

    Update name.py

--- name.py
+++ name.py
@@ -1,2 +1,13 @@
-print("prai")
-print("nila")
+bike = 'Yamaha'
+
+if bike == 'Hero':
+    print("bike is Hero")
+
+elif bike == "Suzuki":
+    print("bike is Suzuki")
+
+elif bike == "Yamaha":
+    print("bike is Yamaha")
+
+else:
+    print("Please choose correct answer")
