cb105bc8d61836cac4bb8bbb2133ac7386076e6e
MoonSyntax <ranjanprainila2809@gmail.com>
2025-03-22 18:40:10 +0530

    Update name.py

--- name.py
+++ name.py
@@ -1,3 +1,9 @@
-print("nila")
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
 
-print("PRAINILA")
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
