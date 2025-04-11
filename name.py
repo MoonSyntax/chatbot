dff17f08c6aef098b8cc8301f0c55da192f7d37e
MoonSyntax <ranjanprainila2809@gmail.com>
2025-04-11 19:38:23 +0530

    Update name.py

--- name.py
+++ name.py
@@ -1,9 +1,6 @@
-class Person:
-  def __init__(self, name, age):
-    self.name = name
-    self.age = age
+def my_function(fname):
+  print(fname + " Refsnes")
 
-p1 = Person("John", 36)
-
-print(p1.name)
-print(p1.age)
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
